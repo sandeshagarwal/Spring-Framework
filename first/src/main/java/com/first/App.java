ff4a42b6268b484290a246d469991ec3d0b07f8e
unknown <sandeshagarwal725@gmail.com>
2020-10-26 21:09:32 +0530

    commit 1

---
+++ first/src/main/java/com/first/App.java
@@ -0,0 +1,24 @@
+package com.first;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        ApplicationContext context =new ClassPathXmlApplicationContext("config.xml");
+        Student s1 = (Student)context.getBean("student1");
+        Student s2 = (Student)context.getBean("student2");
+        Student s3 = (Student)context.getBean("student3");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
